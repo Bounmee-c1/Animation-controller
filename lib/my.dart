82394b4bb1ed47cecbfdc6d0a08c083363bdbfea
Bounmee-c1 <153382541+Bounmee-c1@users.noreply.github.com>
2023-12-19 15:22:01 +0700

    first commit

---
+++ lib/my.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class bounmee extends StatelessWidget {
+  const bounmee({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return bounmee(
+      body: Center(
+        child: Icon(
+          Icons.cake,
+          size: 120.0,
+          color: Colors.orange,
+        ),
+      ),
+    );
+  }
+}
